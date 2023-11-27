97b495c65f49728a6d6af03da49b947f8f954a2a
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-11-27 09:50:38 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Oliver Delaney on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
